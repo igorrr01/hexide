a7cb297414837a3ce37a0da8381db35521e9d3e7
Igor <footking.sup@gmail.com>
2022-10-05 16:17:47 +0300

    #

--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -5,13 +5,11 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="QOhLIaP0qkEK2ORKtdagSRXpW6OWxTutb4YF97rn">
     <title>Admin Panel</title>
     <link rel="stylesheet" href="/public/vendor/fontawesome-free/css/all.min.css">
     <link rel="stylesheet" href="/public/vendor/overlayScrollbars/css/OverlayScrollbars.min.css">
     <link rel="stylesheet" href="/public/vendor/adminlte/dist/css/adminlte.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-    <!--     <link rel="stylesheet" href="/css/admin_custom.css">-->
     <script src="/public/vendor/jquery/jquery.min.js"></script>
     <script src="/public/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
     <script src="/public/vendor/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
